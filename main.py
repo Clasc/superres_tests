b3014f8b2efa7a641e2c0c82c7f8b96d71030f2f
Christian Lascsak <christian.lascsak7@gmail.com>
2021-11-26 18:07:03 +0100

    move flask routes to app.py and use routes in main for gcloud

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-from flask import Flask, request
+from flask.wrappers import Request
 from superres import supperresVideo, videoStream
 
-app = Flask(__name__)
 
-
-@app.route("/")
-def hello_world():
+def hello_world(request: Request):
     return "<h1>Hello, World!</h1>"
 
 
-@app.route("/superres", methods=["POST"])
-def superres():
+def superres(request: Request):
     # print(request.headers)
     video = request.files["video"]
     if video is None:
